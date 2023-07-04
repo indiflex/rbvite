9969170edadb12d26d858a824f2e0361b890cbfb
indiflex <indiflex1@gmail.com>
2023-07-04 22:42:20 +0900

    scope

---
+++ js/lexi_scope.js
@@ -0,0 +1,55 @@
+var gg = 1;
+let bb = 2;
+function f1() {
+	var gg = 11;
+	let bb = 22;
+	console.log('f1>', gg, bb); // ?
+	f2(); // inner
+	const xx = 1;
+	function f2() {
+		// const f2 = () => {  // TDZ 유발!!
+		console.log('inner f2!!');
+	}
+	f2(); // callable? Yes (: 함수도 hoisting)
+}
+
+function f2() {
+	console.log('global f2>', gg, bb, xx, kk); // ?
+}
+
+let xx = 9;
+if (gg > 0) {
+	var kk = 33;
+	const yy = 99;
+}
+f1();
+// f2(); // outer
+// console.log(kk, yy);
+
+function varFn() {
+	var v = 1;
+	{
+		var v = 2,
+			vv = 3;
+		console.log(v, vv); // 2, 3
+	}
+	console.log(v, vv); // 2, 3
+} // v는 하나의 공간 (stack)
+
+function letFn() {
+	let l = 1;
+	{
+		let l = 2,
+			ll = 3;
+		console.log(l, ll); // ?
+	}
+	// console.log(l, ll); // ?
+}
+
+varFn();
+letFn();
+
+// f(); // TDZ (freshness)
+const f = () => console.log('ffffffffff'); // remove fresh state
+
+f();
