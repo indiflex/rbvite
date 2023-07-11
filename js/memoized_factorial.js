69ceeedbe32bf6602e4865d65081e54aea17fd68
indiflex <indiflex1@gmail.com>
2023-07-11 16:37:10 +0900

    aa

--- js/memoized_factorial.js
+++ js/memoized_factorial.js
@@ -1,15 +1,15 @@
-function memoized(fn) {
-  const memoizedTable = {};
-  return function (k) {
-    // return lookupTable[k] || (lookupTable[k] = fn(k));
+// function memoized(fn) {
+//   const memoizedTable = {};
+//   return function (k) {
+//     // return lookupTable[k] || (lookupTable[k] = fn(k));
 
-    if (memoizedTable[k]) return memoizedTable[k];
+//     if (memoizedTable[k]) return memoizedTable[k];
 
-    memoizedTable[k] = fn(k);
-    console.log(k, 'LT>>', memoizedTable);
-    return memoizedTable[k];
-  };
-}
+//     memoizedTable[k] = fn(k);
+//     console.log(k, 'LT>>', memoizedTable);
+//     return memoizedTable[k];
+//   };
+// }
 
 // let runCnt = 0;
 // function factorial(n) {
@@ -21,6 +21,11 @@ function memoized(fn) {
 // console.log(factorial(3));
 // console.log(factorial(5));
 // console.log(`runCnt11=${runCnt}`);
+
+const memoized = fn => {
+  const memoizedTable = {};
+  return k => memoizedTable[k] || (memoizedTable[k] = fn(k));
+};
 
 const memoizedFactorial = memoized(function (n) {
   memoizedFactorialRunCnt++;
