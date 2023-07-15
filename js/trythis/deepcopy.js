a35ac14d9df5489efaba3971dd906708ebd7b769
indiflex <indiflex1@gmail.com>
2023-07-15 17:52:35 +0900

    deepCopy

--- js/trythis/deepcopy.js
+++ js/trythis/deepcopy.js
@@ -1,3 +1,13 @@
+const assert = require('assert');
+
+const arr = [1, 2, 3];
+const hong = { id: 1, name: 'Hong' };
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+const lucy = new Dog('Lucy');
 const kim = {
   nid: 3,
   addr: 'Pusan',
@@ -5,18 +15,60 @@ const kim = {
   oo: { id: 1, name: 'Hong', addr: { city: 'Seoul' } },
   xx: null,
   yy: function () {
-    console.log(this.oo);
+    console.log('yy>>', this.oo);
+  },
+  yyy() {
+    console.log('yyy>>', this.addr);
   },
   [Symbol()]: 9,
   [Symbol()]: Symbol('symbol2'),
   zs: new Set([arr, hong]),
   zws: new WeakSet([arr, hong]),
-  zm: new Map([[1, arr]]),
+  zm: new Map([[hong, arr]]),
   zwm: new WeakMap([[hong, arr]]),
   dog: lucy,
 };
+
+const deepCopy = obj => {
+  if (
+    obj === null ||
+    typeof obj !== 'object' ||
+    obj instanceof WeakSet ||
+    obj instanceof WeakMap
+  )
+    return obj;
+
+  const copiedObj = new obj.constructor();
+  if (obj instanceof Set) {
+    [...obj.keys()].forEach(_k => copiedObj.add(deepCopy(_k)));
+    return copiedObj;
+  } else if (obj instanceof Map) {
+    [...obj.entries()].forEach(([_k, _v]) =>
+      copiedObj.set(deepCopy(_k), deepCopy(_v))
+    );
+    return copiedObj;
+  }
+
+  // const copiedObj = Array.isArray(obj) ? [] : {};
+  for (const k of Reflect.ownKeys(obj)) {
+    copiedObj[k] = deepCopy(obj[k]);
+  }
+
+  return copiedObj;
+};
+
 const newKim = deepCopy(kim);
-assert.deepStrictEqual(newKim, kim, 'deepCopy equal fail!');
+// console.log(kim.zm);
+// console.log('--------------');
+//  = 2;
+for (const mk of newKim.zm.keys()) mk.id = 100;
+for (const sk of newKim.zs.keys()) {
+  console.log('ssss>>', sk);
+  if ('id' in sk) sk.id = 1000;
+  else sk[0] = 2000;
+  // sk = 100;
+}
+// console.log(newKim.zm);
 newKim.addr = 'Daegu';
 newKim.oo.name = 'Kim';
 assert.notDeepStrictEqual(newKim, kim, 'Not Valid Deep Copy!');
@@ -29,4 +81,17 @@ console.log(
   kim.arr[0] !== newKim.arr[0] && kim.arr[3].aid !== newKim.arr[3].aid
 );
 assert.notStrictEqual(kim.arr[4][1], newKim.arr[4][1], 'pass2: 다르지 않아요!');
-console.log('Pass3?', kim.oo.addr.city !== newKim.oo.addr.city);
+assert.notStrictEqual(
+  kim.oo.addr.city,
+  newKim.oo.addr.city,
+  'Not Pass3: city가 다르지 않아요!'
+);
+
+console.log(kim);
+console.log('---------------');
+console.log(newKim);
+console.log('---------------');
+kim.yy();
+newKim.yy();
+kim.yyy();
+newKim.yyy();
